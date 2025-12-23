9c79d934cb0990a0a1ee5bdf309eea975d72a79c
lrq <1632108322@qq.com>
2025-12-23 17:57:51 +0800

    feat: complet the base fetch.

--- src/ai/index.ts
+++ src/ai/index.ts
@@ -1,28 +1,38 @@
 import OpenAI from "openai";
 import { Notice } from "obsidian";
+import SmartSelectPlugin from "main";
+import { apiSetting } from "../type";
+export class AIService {
+	private openai!: OpenAI;
+	private plugin!: SmartSelectPlugin;
+	constructor(plugin: SmartSelectPlugin) {
+		const { key, baseUrl }: apiSetting = plugin.settings.api;
+		this.plugin = plugin;
+		this.openai.apiKey = key;
+		this.openai.baseURL = baseUrl;
+	}
+	async askAI(prompt: string) {
+		const modelSetting = this.plugin.settings.modelSetting;
+		try {
+			const completion = await this.openai.chat.completions.create({
+				messages: [
+					{
+						role: "system",
+						content: modelSetting.systemInstruction,
+					},
+					{ role: "user", content: prompt },
+				],
+				temperature: modelSetting.temperature,
+				model: this.plugin.settings.api.modalName,
+			});
 
-export async function askAI(apiKey: string, prompt: string) {
-	const openai = new OpenAI({
-		baseURL: "https://api.deepseek.com",
-		apiKey: apiKey,
-		dangerouslyAllowBrowser: true, // 必须开启，因为 Obsidian 是浏览器环境
-	});
-
-	try {
-		const completion = await openai.chat.completions.create({
-			messages: [
-				{ role: "system", content: "You are a helpful assistant." },
-				{ role: "user", content: prompt },
-			],
-			model: "deepseek-chat",
-		});
-
-		// 使用可选链操作符避免 "Object is possibly undefined" 错误
-		const content = completion.choices[0]?.message?.content;
-		return content || "";
-	} catch (error) {
-		console.error("AI 请求出错:", error);
-		new Notice("AI 请求出错，请检查控制台");
-		return "";
+			// 使用可选链操作符避免 "Object is possibly undefined" 错误
+			const content = completion.choices[0]?.message?.content;
+			return content || "";
+		} catch (error) {
+			console.error("AI 请求出错:", error);
+			new Notice("AI 请求出错，请检查控制台");
+			return "";
+		}
 	}
 }
