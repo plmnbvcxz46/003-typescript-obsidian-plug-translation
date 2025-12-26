9e26b68bcdb1bdef428c547ef1f12aa5d430937c
lrq <1632108322@qq.com>
2025-12-26 10:48:12 +0800

    feat: 添加右侧视图内数据的数据结构

--- src/type.ts
+++ src/type.ts
@@ -11,3 +11,30 @@ type modelSetting = {
 };
 
 export type { apiSetting, modelSetting };
+
+interface BaseNode {
+	original: string;
+	translation: string;
+}
+
+export interface WordNode extends BaseNode {
+	type: "words";
+	otherMeanings: {
+		translation: string;
+		examples: string;
+	}[];
+}
+
+export interface SentenceNode extends BaseNode {
+	type: "sentence";
+	construction: string;
+	keyWords: WordNode[];
+	keyPhrase: BaseNode[];
+}
+
+export interface ParagraphNode extends BaseNode {
+	type: "paragraph";
+	importantWords: SentenceNode[];
+}
+
+export type AnalysisResult = WordNode | SentenceNode | ParagraphNode | BaseNode;
